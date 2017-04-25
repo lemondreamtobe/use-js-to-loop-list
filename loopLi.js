f2a1c04889c01569c66a3d2e50ae4d48b3f0558f
Lemon_zhang <897418436@qq.com>
2017-04-25 11:12:01 +0800

    loopLi.js
    js

---
+++ loopLi.js
@@ -0,0 +1,10 @@
+function loopLi() {
+				var nowLi = $("#descriptUl li:visible");
+				var nextLi = nowLi.val() == "3" ?  $("#descriptUl li:first"): nowLi.next();
+				setTimeout(function() {
+					nowLi.css("display", "none");
+					nextLi.css("display", "block");					
+					setTimeout(changeLi(),4000);
+				},3000)
+			};
+loopLi();
