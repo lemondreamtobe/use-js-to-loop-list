0a182b2b06a9707e87feac8e47039f576673500d
lemondreamtobe <897418436@qq.com>
2017-05-17 16:01:53 +0800

    改变两个script顺序

--- loopLi.js
+++ loopLi.js
@@ -1,6 +1,7 @@
 function loopLi() {
 				var nowLi = $("#descriptUl li:visible");
 				var nextLi = nowLi.val() == "3" ?  $("#descriptUl li:first"): nowLi.next();
+				console.log(nowLi.text())
 				setTimeout(function() {
 					nowLi.css("display", "none");
 					nextLi.css("display", "block");					
