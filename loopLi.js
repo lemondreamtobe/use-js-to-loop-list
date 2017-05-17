a656609b118fa85ddf9898da2762ecb344f3ada0
lemondreamtobe <897418436@qq.com>
2017-05-17 16:04:47 +0800

    1

--- loopLi.js
+++ loopLi.js
@@ -1,3 +1,6 @@
+$(function(){
+	$("#descriptUl li:not(':first')").css("display", "none");
+}());
 function loopLi() {
 				var nowLi = $("#descriptUl li:visible");
 				var nextLi = nowLi.val() == "3" ?  $("#descriptUl li:first"): nowLi.next();
